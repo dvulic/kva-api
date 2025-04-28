4d936ccfdc20968ef6efb7031e8cd6ab94b1e319
dvulic <dvulic9323rn@raf.rs>
2025-04-28 15:43:31 +0200

    removed package-lock.json set up cors

--- express.js
+++ express.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import trailerRoutes from './routes/trailer.js';
-import gradesRoutes from './routes/grades.js'
-  ;
+import gradesRoutes from './routes/grades.js';
+
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors())
 app.use('/trailer', trailerRoutes);
 app.use('/grade', gradesRoutes);
 
